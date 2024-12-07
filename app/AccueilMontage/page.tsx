0f8b2d61734c1c63ce5e87614b73dee7c4885c4c
Kévin MAZE <kekeproject@hotmail.fr>
2024-12-07 18:29:50 +0100

    begin acceuil montage

--- app/AccueilMontage/page.tsx
+++ app/AccueilMontage/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from './Accueil.module.css'
+import clsx from 'clsx'
+import Image from 'next/image'
+import Logo from '../../public/images/logo.png'
 
 const AccueilMontage = () => {
     return (
-        <div>Accueil Montage</div>
+        <div className='flux'>
+
+            <div className={styles.bg__logo}>
+                <Image src={Logo} alt='Logo Michon Nicolas' />
+            </div>
+
+            <div className={styles.bg__title}>
+                <div className={styles.title}>
+
+                </div>
+            </div>
+
+        </div>
     )
 }
 
