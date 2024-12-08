2e55dde3502cafcbd4599f755e2e82a7dde2a741
Kévin MAZE <kekeproject@hotmail.fr>
2024-12-08 13:43:03 +0100

    add logo loading page

--- app/page.tsx
+++ app/page.tsx
@@ -4,31 +4,35 @@ import Image from "next/image";
 import Logo from "../public/images/logo.png";
 
 export const metadata = {
-  title: "Nicolas Michon",
-  description: "Monteur vidéo et copywriter",
-  icons: {
-    icon: "../public/images/logo.png",
-  },
+    title: "Nicolas Michon",
+    description: "Monteur vidéo et copywriter",
+    icons: {
+        icon: "../public/images/logo.png",
+    },
 };
 
 export default function Home() {
-  return (
-    <section className={styles.container}>
-      <div className={styles.sectionLeft}>
-        <div className={styles.overlay}></div>
-        <div className={styles.buttonWrapper}>
-          <Button label="Montage" href="/AccueilMontage" />
-        </div>
-      </div>
+    return (
+        <section className={styles.container}>
+            <div className={styles.sectionLeft}>
+                <div className={styles.overlay}></div>
+                <div className={styles.buttonWrapper}>
+                    <Button label="Montage" href="/AccueilMontage" />
+                </div>
+            </div>
 
-      <Image src={Logo} alt="Logo Michon Nicolas" className={styles.logo} />
+            <Image
+                src={Logo}
+                alt="Logo Michon Nicolas"
+                className={styles.logo}
+            />
 
-      <div className={styles.sectionRight}>
-        <div className={styles.overlay}></div>
-        <div className={styles.buttonWrapper}>
-          <Button label="Copywriting" href="/AccueilCopy" />
-        </div>
-      </div>
-    </section>
-  );
+            <div className={styles.sectionRight}>
+                <div className={styles.overlay}></div>
+                <div className={styles.buttonWrapper}>
+                    <Button label="Copywriting" href="/AccueilCopy" />
+                </div>
+            </div>
+        </section>
+    );
 }
