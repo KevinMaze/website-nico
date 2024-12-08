d9dfff9785f8628b02c247049316dad3df9eba3b
Kévin MAZE <kekeproject@hotmail.fr>
2024-12-08 10:33:26 +0100

    add div acceuil

--- app/AccueilMontage/page.tsx
+++ app/AccueilMontage/page.tsx
@@ -3,6 +3,7 @@ import styles from './Accueil.module.css'
 import clsx from 'clsx'
 import Image from 'next/image'
 import Logo from '../../public/images/logo.png'
+import ImgTitle from '../../public/images/bg-montage.jpg' 
 
 const AccueilMontage = () => {
     return (
@@ -13,13 +14,29 @@ const AccueilMontage = () => {
             </div>
 
             <div className={styles.bg__title}>
+
                 <div className={clsx (styles.title, 'flux')}>
+                    <h2>title</h2>
+
+                    <div className={styles.title__div}>
+                        <p>Lorem ipsum dolor sit amet. Qui molestiae animi et omnis dolorem qui dignissimos fugit in nostrum velit. Nam expedita voluptatem est adipisci corporis aut modi facere eos velit quod et numquam laboriosam sit galisum minus. Est reiciendis sint et rerum sunt est recusandae quisquam id voluptate voluptatibus et iure assumenda et dolorem suscipit! Ea similique nihil et eligendi tenetur ut velit aliquid non aperiam nostrum aut debitis incidunt 33 velit ipsum.
+                        At consectetur fuga sed dolore reprehenderit et dolores voluptatibus qui nisi incidunt ut animi maiores. At debitis delectus aut recusandae sapiente quo temporibus consequatur ut saepe necessitatibus et officiis autem sed deleniti perferendis aut iusto esse. Et consectetur provident eos tempore quia hic beatae cumque in consequuntur voluptas ut reprehenderit quasi in tempore dignissimos.
+                        Vel debitis sint sit autem asperiores est voluptate delectus aut dolore odio cum minima officia ut quia culpa! Qui placeat omnis et eaque odio aut nesciunt officia ex error internos. Est natus laborum et expedita tempora eos minus eius. Et eius corporis est quidem quidem aut quasi quaerat et quia nostrum ad tempora pariatur ut animi quibusdam sed perspiciatis Quis.</p>
+                        <Image src={ImgTitle} alt='Template de montage' className={styles.imgTitle} />
+                    </div>
 
                 </div>
+
             </div>
 
             <div className={styles.bg__gallery}>
+                
                 <div className={clsx (styles.gallery, 'flux')}>
+                    <h2>gallery</h2>
+
+                    <div className={styles.gallery__div}>
+
+                    </div>
 
                 </div>
             </div>
